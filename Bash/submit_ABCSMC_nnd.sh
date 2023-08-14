14b7c71b793b27d85be822e8f4d0a53d2301281f
Tianjian <tianjianqin@outlook.com>
2023-08-14 14:32:13 +0200

    Reduce resource requirement

--- Bash/submit_ABCSMC_nnd.sh
+++ Bash/submit_ABCSMC_nnd.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --time=7-2:00:00
+#SBATCH --time=3-2:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=16
 #SBATCH --job-name=edd_ABCSMC_nnd
 #SBATCH --output=logs/edd_ABCSMC_nnd-%j.log
-#SBATCH --mem=128GB
+#SBATCH --mem=32GB
 #SBATCH --partition=regular
 #SBATCH --mail-type=ALL
 
--- Bash/submit_ABCSMC_pd_nr.sh
+++ Bash/submit_ABCSMC_pd_nr.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=7-2:00:00
+#SBATCH --time=3-2:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=16
-#SBATCH --job-name=edd_ABCSMC_pd
-#SBATCH --output=logs/edd_ABCSMC_pd-%j.log
+#SBATCH --job-name=edd_ABCSMC_pd_nr
+#SBATCH --output=logs/edd_ABCSMC_pd_nr-%j.log
 #SBATCH --mem=32GB
 #SBATCH --partition=regular
 #SBATCH --mail-type=ALL
